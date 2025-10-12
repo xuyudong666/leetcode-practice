66b833972b2893d016aa174ec082af0b8206ca22
yudong <xuyudong.666@qq.com>
2025-10-12 12:07:30 +0800

    Initial commit: add LeetCode practice project

---
+++ LeetCodeSample.cs
@@ -0,0 +1,100 @@
+
+namespace leetcode;
+
+public class LeetCodeSample
+{
+    public int ScoreOfString(string s)
+    {
+        int result = 0;
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            result += Math.Abs(s[i] - s[i - 1]);
+        }
+
+        return result;
+    }
+
+    public IList<int> StableMountains(int[] height, int threshold)
+    {
+        List<int> result = [];
+
+        for (int i = 1; i < height.Length; i++)
+        {
+            if (height[i - 1] > threshold)
+            {
+                result.Add(i);
+            }
+        }
+
+        return result;
+    }
+
+    public int MinOperations(int[] nums, int k)
+    {
+        int max = 0;
+
+        foreach (var num in nums)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        return max % k;
+    }
+
+    public int SubarraySum(int[] nums)
+    {
+        int result = 0;
+        int n = nums.Length;
+        int[] prefix = new int[n + 1];
+        for (int j = 0; j < n; j++)
+        {
+            prefix[j + 1] = prefix[j] + nums[j];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int start = Math.Max(0, i - nums[i]);
+            result += prefix[i + 1] - prefix[start];
+        }
+
+        return result;
+    }
+
+    public int[] MinCosts(int[] cost)
+    {
+        int[] result = new int[cost.Length];
+        int minCost = int.MaxValue;
+        for (int i = 0; i < cost.Length; i++)
+        {
+            if (cost[i] < minCost)
+            {
+                minCost = cost[i];
+                result[i] = minCost;
+            }
+            else
+            {
+                result[i] = minCost;
+            }
+        }
+
+        return result;
+    }
+
+    public int EvenNumberBitwiseORs(int[] nums)
+    {
+        int result = 0;
+        foreach (var num in nums)
+        {
+            if ((num & 1) == 0)
+            {
+                result |= num;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using leetcode;
+
+// string s = "zaz";
+
+ LeetCodeSample  leetCodeSample = new LeetCodeSample();
+
+// void TestSubarraySum(int[] nums, string label, int expected)
+// {
+//     var val = leetCodeSample.SubarraySum(nums);
+//     Console.WriteLine($"{label} nums=[{string.Join(",", nums)}] -> {val} (期望={expected})");
+// }
+
+// TestSubarraySum(new[] { 2, 3, 1 }, "SubarraySum 用例1", 11);
+// TestSubarraySum(new[] { 3, 1, 1, 2 }, "SubarraySum 用例2", 13);
+
+// void TestMinCosts(int[] cost, string label, int[] expected)
+// {
+//     var res = leetCodeSample.MinCosts(cost);
+//     Console.WriteLine($"{label} cost=[{string.Join(",", cost)}] -> [{string.Join(",", res)}] (期望=[{string.Join(",", expected)}])");
+// }
+
+// TestMinCosts(new[] { 5, 3, 4, 1, 3, 2 }, "MinCosts 示例1", new[] { 5, 3, 3, 1, 1, 1 });
+// TestMinCosts(new[] { 1, 2, 4, 6, 7 }, "MinCosts 示例2", new[] { 1, 1, 1, 1, 1 });
+
+
+void TestEvenOr(int[] nums, string label, int expected)
+{
+    var res = leetCodeSample.EvenNumberBitwiseORs(nums);
+    Console.WriteLine($"{label} nums=[{string.Join(",", nums)}] -> {res} (期望={expected})");
+}
+
+TestEvenOr(new[] { 1, 2, 3, 4, 5, 6 }, "EvenNumberBitwiseORs 示例1", 6);
+TestEvenOr(new[] { 7, 9, 11 },          "EvenNumberBitwiseORs 示例2", 0);
+TestEvenOr(new[] { 1, 8, 16 },          "EvenNumberBitwiseORs 示例3", 24);
